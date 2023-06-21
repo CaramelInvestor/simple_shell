0740e853e10fd84b49f66e6a4b4205cebcb4b4a2
Nellydhlamini <nellydhlamini34@gmail.com>
2023-06-21 16:12:50 -0700

    Prints a string

--- _printf.c
+++ _printf.c
@@ -9,17 +9,45 @@
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
+va_list args;
+va_start(args, format);
 
-	while (*format != '\0') /*as long as the characters are not null byte*/
-	{
-		/*Print any other character*/
-		_putchar(*format);
-		format++;
-	}
+while (*format != '\0') /*as long as the characters are not null byte*/
+{
+if (*format == '%') /*note when the program encounters a "%"*/
+{
+format++; /*Move past '%'*/
+if (*format == 'c')
+{
+/*print a character*/
+int c = va_arg(args, int);
+_putchar(c);
+}
+else if (*format == 's')
+{
+/*print a string*/
+char *s = va_arg(args, char *);
+while (*s != '\0')
+{
+_putchar(*s);
+s++;
+}
+}
+else if (*format == '%')
+{
+/*Print '%'*/
+_putchar('%');
+}
+}
+else
+{
+/*Print any other character*/
+_putchar(*format);
+}
+format++;
+}
 
-	va_end(args);
+va_end(args);
 
-	return (0);
+return (0);
 }
